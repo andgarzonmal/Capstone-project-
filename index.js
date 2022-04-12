758ccf2f6a44d2a59132bb7b59f4146411bbe46b
andgarzonmal <andgarzonmal@unal.edu.co>
2022-04-11 20:55:11 -0500

    ➕ ADD display menu feature

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
-const menu = document.getElementById("bar2");
-const displayMenu = document.getElementById("menu-icon");
-const 
+const menu = document.querySelector(".bar2");
+const displayMenu = document.querySelector(".menu-icon");
+const header = document.querySelector(".header");
+const listItems = document.querySelector(".nav-list");
+const clear = document.querySelector(".clear");
 
-function () {
+
+function displayMyMenu() {
+  header.classList.add('header-menu');
+  header.classList.remove('header');
+  
+  menu.classList.add("my-menu");
+  menu.classList.remove("bar2");
+
+  listItems.classList.add("my-menu-list")
+  listItems.classList.remove("nav-list")
+
+  clear.style.display="unset"
+  console.log(65654654)
+
+}
+
+displayMenu.addEventListener("click", displayMyMenu)
+
+function exit() {
+  header.classList.remove('header-menu');
+  header.classList.add('header');
+  
+  menu.classList.remove("my-menu");
+  menu.classList.add("bar2");
+
+  listItems.classList.remove("my-menu-list")
+  listItems.classList.add("nav-list")
+  
+  clear.style.display="none"
   
 }
+
+clear.addEventListener("click", exit)
