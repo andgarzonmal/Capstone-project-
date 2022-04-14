c35f8125107459b87253ceb3e5b2a6989f89f23c
andgarzonmal <andgarzonmal@unal.edu.co>
2022-04-14 07:32:07 -0500

    🪛 FIX linters

--- index.js
+++ index.js
@@ -34,59 +34,61 @@ function exit() {
 
 clear.addEventListener('click', exit);
 
-const soloists = document.querySelector(".soloists-cards") 
+const soloists = document.querySelector('.soloists-cards');
 
 const invitedSoloists = [
   {
-    id: "card6",
-    soloist: "Hilary Hahn",
-    photo:"./images/hilary.jpg",
-    description: " She has performed throughout the world as a soloist with leading orchestras and conductors and as a recitalist. ",
-    instrument: "Violin"
+    id: 'card6',
+    soloist: 'Hilary Hahn',
+    photo: './images/hilary.jpg',
+    description: ' She has performed throughout the world as a soloist with leading orchestras and conductors and as a recitalist. ',
+    instrument: 'Violin',
   },
 
   {
-    id: "card5",
-    soloist: "Yuja Wang",
-    photo:"./images/yuha.jpg",
-    description: "is a Chinese classical pianist. She was born in Beijing, began studying piano there at age six, and went on to study at the Central Conservatory of Music in Beijing.",
-    instrument: "Piano"
+    id: 'card5',
+    soloist: 'Yuja Wang',
+    photo: './images/yuha.jpg',
+    description: 'is a Chinese classical pianist. She was born in Beijing, began studying piano there at age six, and went on to study at the Central Conservatory of Music in Beijing.',
+    instrument: 'Piano',
   },
 
   {
-    cardid: "card4",
-    soloist: "Pacho flores",
-    photo:"./images/pacho4.jpg",
-    description: "Pacho Flores was awarded First Prize in the “Maurice André” International Contest, the most renowned trumpet Contest in the world.",
-    instrument: "Trumpet"
-  },
-
-  { 
-    cardid: "card3",
-    soloist: "Mischa Maisky",
-    photo:"./images/MischaMaisky-2.jpg",
-    description: "He was taught by Mstislav Rostropovich at the Moscow Conservatory from 1966 to 1970. In 1966, he won sixth prize at the in the most famous Cello contest.",
-    instrument: "Cello"
+    cardid: 'card4',
+    soloist: 'Pacho flores',
+    photo: './images/pacho4.jpg',
+    description: 'Pacho Flores was awarded First Prize in the “Maurice André” International Contest, the most renowned trumpet Contest in the world.',
+    instrument: 'Trumpet',
   },
 
   {
-    cardid: "card2",
-    soloist: "Manuel Barrueco",
-    photo:"./images/Manuel.jpg",
-    description: "Manuel Barrueco is a Cuban classical guitarist. During three decades of concert performances he has performed and recorded across the United States ",
-    instrument: "Guitar"
+    cardid: 'card3',
+    soloist: 'Mischa Maisky',
+    photo: './images/MischaMaisky-2.jpg',
+    description: 'He was taught by Mstislav Rostropovich at the Moscow Conservatory from 1966 to 1970. In 1966, he won sixth prize at the in the most famous Cello contest.',
+    instrument: 'Cello',
   },
 
   {
-    cardid: "card1",
-    soloist: "itzhak perlman",
-    photo:"./images/itzhak.jpg",
-    description: "Is an Israeli-American violinist, conductor, and music teacher. Perlman has performed worldwide, and throughout the United States",
-    instrument: "Violin"
-  }
-]
+    cardid: 'card2',
+    soloist: 'Manuel Barrueco',
+    photo: './images/Manuel.jpg',
+    description: 'Manuel Barrueco is a Cuban classical guitarist. During three decades of concert performances he has performed and recorded across the United States ',
+    instrument: 'Guitar',
+  },
 
-const Card = ({soloist, photo, description, id, instrument}) =>`
+  {
+    cardid: 'card1',
+    soloist: 'itzhak perlman',
+    photo: './images/itzhak.jpg',
+    description: 'Is an Israeli-American violinist, conductor, and music teacher. Perlman has performed worldwide, and throughout the United States',
+    instrument: 'Violin',
+  },
+];
+
+const Card = ({
+  soloist, photo, description, id, instrument,
+}) => `
   <div id="${id}" class="cards"> 
     <div> 
       <img class="cards-img" src="${photo}" alt="hillary">
@@ -100,6 +102,4 @@ const Card = ({soloist, photo, description, id, instrument}) =>`
 
 invitedSoloists.forEach((object) => {
   soloists.insertAdjacentHTML('afterbegin', Card(object));
-  const currentCard = document.querySelector(`#${object.id}`);
 });
-
